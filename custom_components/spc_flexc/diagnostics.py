d2d7e577da704f657acbcb9f2e54c87cb81479ca
minimicro34 <minimicro34@gmail.com>
2026-09-07 12:34:11 +0200

    Include discovered doors and communication paths in diagnostics

--- custom_components/spc_flexc/diagnostics.py
+++ custom_components/spc_flexc/diagnostics.py
@@ -5,5 +5,8 @@ async def async_get_config_entry_diagnostics(hass, entry):
         "panel": c.data.panel.raw,
         "areas": c.data.areas,
         "zones": c.data.zones,
+        "doors": c.data.doors,
+        "ats": c.data.ats,
+        "xbus_devices": c.data.xbus_devices,
         "faults": vars(c.data.faults),
     }
